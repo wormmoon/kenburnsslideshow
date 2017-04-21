c72a23f3994de25282e8160734facb33fe2e4298
Emily Oliver <mleeoliver@gmail.com>
2017-04-21 18:31:30 +0100

    Declare vars and kenBurns function

--- js/script.js
+++ js/script.js
@@ -5,5 +5,17 @@ function() {
 	window.setInterval(kenBurns, 4000);
 	//runs kenBurns function every 4 secs
 
-	
+	var images = document.getElementById("container").getElementsByTagName("img"),
+		numberOfImages = images.length,
+		i = 1;
+
+	function kenBurns() {
+		if(i==numberOfImages) { i = 0; }
+		images[i].className = "transition";
+
+		if(i===0){ images[numberOfImages-2].className = ""; }
+		if(i===1){ images[numberOfImages-1].className = ""; }
+		if(i>1){ images[i-2].className = "";}
+		i++;
+	}
 }
