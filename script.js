90047ea6eaaff3e71fb0fddac3393d0643f72bba
Emily Oliver <mleeoliver@gmail.com>
2017-04-21 18:34:45 +0100

    Move script.js file /again/ because I accidentally saved to it lol

---
+++ script.js
@@ -0,0 +1,6 @@
+function() {
+	//first target the first image in HTML source and give it the class 'transition'
+	document.getElementById("container").getElementsByTagName("img")[0].className = "transition";
+
+	
+}
