5ba5963a0c5f6cd46ab5fb73295e47b8cd298a69
Emily Oliver <mleeoliver@gmail.com>
2017-04-21 18:30:24 +0100

    Run kenBurns function every 4 secs

--- js/script.js
+++ js/script.js
@@ -2,5 +2,8 @@ function() {
 	//first target the first image in HTML source and give it the class 'transition'
 	document.getElementById("container").getElementsByTagName("img")[0].className = "transition";
 
+	window.setInterval(kenBurns, 4000);
+	//runs kenBurns function every 4 secs
+
 	
 }
